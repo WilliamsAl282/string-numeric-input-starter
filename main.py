e28c407c172defdb989eb612fcf744a105a4c773
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-09-13 11:19:37 -0400

    Updated

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
-# Student name
-# Current date
+# Alex Williams
+# 9/13/2024
 # String & Numeric Input 
 
 # Task 1
 # Comment
 # Comment
+name = input('What is your name?:\n')
+program = input('What program are you in?:\n')
 
+# # print(f'{name}' + " " + 'is taking' + " " + {program})
+print(name + ' is taking ' + program + '.')
 
 
 
 # Task 2
 # Comment
 # Comment
-
+age = input('What is your age?:\n')
+age = int(age)
+print(f'My age is {age}.')
+new_age = age + 10
+print(f'My age in 10 years from now will be {new_age}')
 
 
 
 # Task 3
 # Comment
 # Comment
-
+family = input('How many family members do you have?\n')
+family = int(family)
+print('You have ' + str(family) + ' family members in your family')
+print(f'You have {str(family)} family members')
 
 
 
 # Task 4
 # Comment
 # Comment
+height = float(input('What is your height in inches\n'))
+CONVERSION_FACTOR = 2.54
+height_cent = CONVERSION_FACTOR*height
+print('Your height in inches is ' + height + 'and your height in centimeters is' + height_cent)
 
 
 
